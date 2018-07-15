d6c515932df215d26b9cbe607c63a64a3c01be61
Juan Story <juanstory@gmail.com>
2018-07-15 19:23:41 -0300

    Add API 27 compatibility

--- android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
+++ android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
@@ -4,6 +4,7 @@
 package com.corbt.keepawake;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.WindowManager;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -12,48 +13,68 @@ import com.facebook.react.bridge.ReactMethod;
 
 public class KCKeepAwake extends ReactContextBaseJavaModule {
 
-    public KCKeepAwake(ReactApplicationContext reactContext) {
-        super(reactContext);
-    }
+	public KCKeepAwake(ReactApplicationContext reactContext) {
+		super(reactContext);
+	}
 
-    @Override
-    public String getName() {
-        return "KCKeepAwake";
-    }
+	@Override
+	public String getName() {
+		return "KCKeepAwake";
+	}
 
-    @ReactMethod
-    public void activate() {
-        final Activity activity = getCurrentActivity();
+	@ReactMethod
+	public void activate() {
+		final Activity activity = getCurrentActivity();
 
-        if (activity != null) {
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    activity.getWindow().addFlags(
-                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-					    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    );
-                }
-            });
-        }
-    }
+		if (activity != null) {
+			activity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if (android.os.Build.VERSION.SDK_INT >= 27) {
+						activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+						activity.setShowWhenLocked(true);
+						activity.setTurnScreenOn(true);
+						Log.i("KCKeepAwake", "activate(): Added KEEP_SCREEN_ON flag and api 27 methods");
+					} else {
+						activity.getWindow().addFlags(
+							WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+							WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+							WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+						);
+						Log.i("KCKeepAwake", "activate(): Added window flags");
+					}
+				}
+			});
+		} else {
+			Log.e("KCKeepAwake", "activate(): No current activity found");
+		}
+	}
 
-    @ReactMethod
-    public void deactivate() {
-        final Activity activity = getCurrentActivity();
+	@ReactMethod
+	public void deactivate() {
+		final Activity activity = getCurrentActivity();
 
-        if (activity != null) {
-            activity.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    activity.getWindow().clearFlags(
-                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
-					    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    );
-                }
-            });
-        }
-    }
+		if (activity != null) {
+			activity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if (android.os.Build.VERSION.SDK_INT >= 27) {
+						activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+						activity.setShowWhenLocked(false);
+						activity.setTurnScreenOn(false);
+						Log.i("KCKeepAwake", "deactivate(): Removed KEEP_SCREEN_ON flag and api 27 methods");
+					} else {
+						activity.getWindow().clearFlags(
+							WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+							WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+							WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+						);
+						Log.i("KCKeepAwake", "deactivate(): Removed window flags");
+					}
+				}
+			});
+		} else {
+			Log.e("KCKeepAwake", "deactivate(): No current activity found");
+		}
+	}
 }
